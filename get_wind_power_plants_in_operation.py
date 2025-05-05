4cac7acfc4df217a433712b0a3e2ed3dbd65e14a
carlandreasveie <106742101+carlandreasveie@users.noreply.github.com>
2025-05-05 15:37:33 +0200

    Update get_wind_power_plants_in_operation.py
    Column 'Eiere' has been temporary removed. 
    Code is updated to handle this.

--- get_wind_power_plants_in_operation.py
+++ get_wind_power_plants_in_operation.py
@@ -11,21 +11,16 @@ def get_wind_power_plants_in_operation():
     #convert to pandas dataframe, write to Excel
     df=pd.DataFrame(data)
     df1=df.copy()
-    del df1["Eiere"], df1["Turbiner"]
+
     df1.to_excel("wind_power_plants_in_operation.xlsx", index=False)
+    # del df1["Eiere"], df1["Turbiner"]
+    del df1["Turbiner"]
 
-    #with one row for each owner
-    df2 = df.explode('Eiere')
-    #convert dict to columns
-    df2 = pd.concat([df2.drop(['Eiere',"HovedEierNavn","HovedEierOrgNr","Turbiner"], axis=1), df2['Eiere'].apply(pd.Series)], axis=1)
-
-    df2.to_excel("wind_power_plants_in_operation_owners.xlsx", index=False)
-
-    #with one row for each turbine group
-    df3 = df.explode('Turbiner')
-    #convert dict to columns
-    df3 = pd.concat([df3.drop(['Turbiner',"Eiere"], axis=1), df3['Turbiner'].apply(pd.Series)], axis=1)
-    df3.to_excel("wind_power_plants_in_operation_turbines.xlsx", index=False)
+    # with one row for each turbine group
+    df2 = df.explode('Turbiner')
+    # convert dict to columns
+    df2 = pd.concat([df2.drop(['Turbiner'], axis=1), df2['Turbiner'].apply(pd.Series)], axis=1)
+    df2.to_excel("wind_power_plants_in_operation_turbines.xlsx", index=False)
 
 if __name__ == '__main__':
     get_wind_power_plants_in_operation()
